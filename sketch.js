a266238f7132fcb4b834605ec906931fc22eb9d9
JAKE CHAPUT <JWC21@FSU.EDU>
2024-09-05 11:50:48 -0400

    changed ellipse 1 size

--- sketch.js
+++ sketch.js
@@ -30,7 +30,7 @@ function draw() {
   }
   y1 = y1 + y1Speed
    
-  ellipse(mouseX, mouseY, 33, 33);
+  ellipse(mouseX, mouseY, 120, 120);
 
   // if x hits the right side or the left side of the canvas,
   // switch directions...
