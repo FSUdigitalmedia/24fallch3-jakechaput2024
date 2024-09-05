6e7e3e39847135ff212acafeeb4986ed3027fcac
JAKE CHAPUT <JWC21@FSU.EDU>
2024-09-04 20:14:45 -0400

    balls work! figuring out how to see them both

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let x1 = 100;     // x position of the ball
 let y1 = 0;       // y position of the ball
 let x1Speed = 3;  // speed in X direction
 let y1Speed = 7; // speed in Y direction
-let d = 20;      // diameter of the ball
+let d1 = 20;      // diameter of the ball
 
 function setup() {
   createCanvas(400, 400);
@@ -24,5 +24,34 @@ function draw() {
   }
   y1 = y1 + y1Speed;
 
-  ellipse(x1, y1, d);
+  ellipse(x1, y1, d1);
+}
+
+let x2 = 100;     // x position of the ball
+let y2 = 0;       // y position of the ball
+let x2Speed = 2;  // speed in X direction
+let y2Speed = 2; // speed in Y direction
+let d2 = 20;      // diameter of the ball
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  //background(220); // try commenting this out!
+ 
+  // if x hits the right side or the left side of the canvas,
+  // switch directions...
+  if (x2 > width || x2 < 0) {
+    x2Speed = x2Speed * -1;  
+  }
+  x2 = x2 + x2Speed; // adding a negative xSpeed *decreases* x, right?
+  
+  // same as above, but for the top and bottom of the canvas
+  if (y2 > width || y2 < 0) {
+    y2Speed = y2Speed * -1;  
+  }
+  y2 = y2 + y2Speed;
+
+  ellipse(x2, y2, d2);
 }
