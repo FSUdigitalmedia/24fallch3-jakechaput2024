a28303f2f25510bef8201df4a1d73d7e3780c1bf
JAKE CHAPUT <JWC21@FSU.EDU>
2024-09-05 12:29:51 -0400

    ellipse  now changes color on collision

--- sketch.js
+++ sketch.js
@@ -20,27 +20,28 @@ function draw() {
   // if x hits the right side or the left side of the canvas,
   // switch directions...
   if (x1 > width || x1 < 0) {
-    x1Speed = x1Speed * -1;  
+    x1Speed = x1Speed * -1; 
+    fill(random(250), random(250), random(250)); //changes colors for ellipse 1 when it hits walls
   }
   x1 = x1 + x1Speed; // adding a negative xSpeed *decreases* x, right?
   
   // same as above, but for the top and bottom of the canvas
   if (y1 > width || y1 < 0) {
-    y1Speed = y1Speed * -1;  
+    y1Speed = y1Speed * -1; 
+    fill(random(250), random(250), random(250)); //changes colors for ellipse 1 when it hits walls
   }
   y1 = y1 + y1Speed
    
-  ellipse(mouseX, mouseY, 120, 120) //120,120 is cursor ellipse diameter
-  fill(random(250), random(250), random(250)); //this changes both ellipse colors but it looks fugly
+  ellipse(mouseX, mouseY, 120, 120) //ellpse 1 to cursor; 120,120 is cursor ellipse diameter
+   //this changes both ellipse colors but it looks fugly
     
   // if x hits the right side or the left side of the canvas,
   // switch directions...
   if (x2 > width || x2 < 0) {
     x2Speed = x2Speed * -1;  
   }
-  x2 = x2 + x2Speed; // adding a negative xSpeed *decreases* x, right?
-  
-  // same as above, but for the top and bottom of the canvas
+  x2 = x2 + x2Speed; 
+
   if (y2 > width || y2 < 0) {
     y2Speed = y2Speed * -1;  
   }
